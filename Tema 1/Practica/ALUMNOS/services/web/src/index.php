384f7c82e813b08200a0c4af926cf7f336d2cf3b
Rubencalii <rubencorralromero2018@gmail.com>
2025-10-02 14:21:50 +0200

    Hasta el punto 4

---
+++ Tema 1/Practica/ALUMNOS/services/web/src/index.php
@@ -0,0 +1,8 @@
+<?php
+$conn = new mysqli("db", $_ENV['MYSQL_ROOT_USER'] ?? "root", $_ENV['MYSQL_ROOT_PASSWORD'] ?? "rootpass", $_ENV['MYSQL_DATABASE'] ?? "testdb");
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
+$result = $conn->query("SELECT mensaje FROM mensajes");
+$row = $result->fetch_assoc();
+echo "Mensaje desde la BD: " . $row["mensaje"];
